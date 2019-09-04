e2d1b6c93db55ad15e13882a2cc453ab204da5fb
GOLVERA <olveratelcel@gmail.com>
2019-09-04 02:00:19 -0500

    Updates

---
+++ precache-manifest.5b9130ccc44c051daa08c6964919e328.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d4c9293194303a0e60659db489ad333",
+    "url": "/moneylp/index.html"
+  },
+  {
+    "revision": "6db289b6a4625f42e1fa",
+    "url": "/moneylp/static/css/main.ce8b015b.chunk.css"
+  },
+  {
+    "revision": "6fe725ca3113d03fb3a0",
+    "url": "/moneylp/static/js/2.ed2a85db.chunk.js"
+  },
+  {
+    "revision": "6db289b6a4625f42e1fa",
+    "url": "/moneylp/static/js/main.d3cc967c.chunk.js"
+  },
+  {
+    "revision": "18779de9ca055f2f0bb4",
+    "url": "/moneylp/static/js/runtime~main.01cbe614.js"
+  }
+]);
